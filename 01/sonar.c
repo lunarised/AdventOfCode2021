afbcb30abce37014693b94492836c02c67cef986
lunarised <lunarised@outlook.com>
2021-12-01 22:34:47 +1300

    part2Completed

--- 01/sonar.c
+++ 01/sonar.c
@@ -22,8 +22,39 @@ int partOne()
     }
     return ups;
 }
+int partTwo()
+{
+    int window[3] = {0, 0, 0};
+    int count = 0;
+    char line[100];
+    int ups = 0;
+    FILE *in_file = fopen("input.text", "r");
+
+    fgets(line, 100, in_file);
+    window[0] = atoi(line);
+    fgets(line, 100, in_file);
+    window[1] = atoi(line);
+    fgets(line, 100, in_file);
+    window[2] = atoi(line);
+    int previousNumber = window[0] + window[1] + window[2];
+    int currentNumber;
+
+    while (fgets(line, 100, in_file) != NULL)
+    {
+        window[count % 3] = atoi(line);
+        count++;
+        currentNumber = window[0] + window[1] + window[2];
+        if (previousNumber < currentNumber)
+        {
+            ups++;
+        }
+        previousNumber = currentNumber;
+    }
+    return ups;
+}
 
 int main()
 {
-    printf("%d", partOne());
+    printf("%d\n", partOne());
+    printf("%d\n", partTwo());
 }
