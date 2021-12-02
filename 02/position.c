afabf85c82f14103249c01b9e00d90660069ce13
lunarised <lunarised@outlook.com>
2021-12-02 22:58:08 +1300

    Part2 done. Turns out memorry allocation is for chumps

--- 02/position.c
+++ 02/position.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main() {
+int partOne() {
   int xPos = 0;
   int yPos = 0;
   int value;
@@ -23,7 +23,37 @@ int main() {
       yPos -= value;
     }
   }
-  printf("%d", abs(xPos * yPos));
+  return abs(xPos * yPos);
+}
 
+int partTwo() {
+  int xPos = 0;
+  int yPos = 0;
+  int aim = 0;
+  int value;
+  char line[100];
+  char* command;
+  FILE* in_file = fopen("input.text", "r");
+
+  while (fgets(line, 100, in_file) != NULL) {
+    command = strtok(line, " ");
+    value = atoi(strtok(NULL, " "));
+    if (strcmp(command, "forward") == 0) {
+      xPos += value;
+      yPos += (aim * value);
+    }
+    if (strcmp(command, "up") == 0) {
+      aim += value;
+    }
+    if (strcmp(command, "down") == 0) {
+      aim -= value;
+    }
+  }
+  return abs(xPos * yPos);
+}
+
+int main() {
+  printf("%d\n", partOne());
+  printf("%d\n", partTwo());
   return EXIT_SUCCESS;
 }
