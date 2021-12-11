b7b4781732ca48a7d33fe3b9754d9603a94aa9ba
lunarised <lunarised@outlook.com>
2021-12-11 23:39:47 +1300

    Cygwin complaining...

---
+++ 10/bracketBalancer.c
@@ -0,0 +1,62 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define INPUT "test.text"
+struct Stack {
+  int top;
+  unsigned capacity;
+  int* array;
+};
+
+struct Stack* createStack(unsigned capacity) {
+  struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+  stack->capacity = capacity;
+  stack->top = -1;
+  stack->array = (int*)malloc(stack->capacity * sizeof(int));
+  return stack;
+}
+
+int isFull(struct Stack* stack) { return stack->top == stack->capacity - 1; }
+
+int isEmpty(struct Stack* stack) { return stack->top == -1; }
+
+void push(struct Stack* stack, int item) {
+  if (isFull(stack)) return;
+  stack->array[++stack->top] = item;
+  printf("%d pushed to stack\n", item);
+}
+
+int pop(struct Stack* stack) {
+  if (isEmpty(stack)) return INT_MIN;
+  return stack->array[stack->top--];
+}
+
+int peek(struct Stack* stack) {
+  if (isEmpty(stack)) return INT_MIN;
+  return stack->array[stack->top];
+}
+
+int partOne() {
+  char* line[500];
+  int i = 0;
+  struct Stack* synStack = createStack(500);
+  FILE* in_file = fopen(INPUT, "r");
+
+  if (in_file == NULL) {
+    printf("File doesnt exist");
+  }
+  while (fgets(line, 500, in_file) != NULL) {
+    int len = (int)strlen(line);
+    for (i = 0; i < len; i++) {
+      printf("%c");
+    }
+    printf("\n");
+  }
+  return 1;
+}
+
+int main() {
+  printf("%d", partOne());
+  return EXIT_SUCCESS;
+}
