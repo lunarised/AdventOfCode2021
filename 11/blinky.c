b79c6fc9b9ef0a80141828b00c9fe8b2934d10bd
lunarised <lunarised@outlook.com>
2021-12-13 17:45:05 +1300

    Day 11 done. Ansi C passing. Fucking not looking forward to 12

--- 11/blinky.c
+++ 11/blinky.c
@@ -1,125 +1,248 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define maxY 5
-#define maxX 5
-#define INPUT "tiny.text"
-#define nSTEPS 1
+#define maxY 10
+#define maxX 10
+#define INPUT "./input.text"
+#define nSTEPS 100
+
+struct Octopus{
+  int value;
+  int isFlashing;
+};
 int partOne() {
   char line[500];
-  int dumboOctopuses[maxY][maxX];
+  struct Octopus dumboOctopuses[maxY][maxX];
   int i = 0;
   int j = 0;
   int step = 0;
+  int flashes = 0;
+  struct Octopus o;
   FILE* in_file = fopen(INPUT, "r");
 
   if (in_file == NULL) {
-    printf("File doesnt exist");
+    printf("File doesnt exist\n");
+    return 1;
   }
   while (fgets(line, 500, in_file) != NULL) {
     for (i = 0; i < maxX; i++) {
-      dumboOctopuses[j][i] = line[i] - '0';
-    }
+      o.value = line[i] - '0';
+      o.isFlashing = 0;
+      dumboOctopuses[j][i] = o;
+    } 
     j++;
-  }
+  } 
+
+
   for (step = 0; step < nSTEPS; step++) {
     int flashing = 0;
     int i;
     int j;
-    for (int i = 0; i < maxY; i++) {
-      for (int j = 0; j < maxX; j++) {
-        dumboOctopuses[j][i] += 1;
-        /*if (dumboOctopuses[j][i] > 9) {
-          dumboOctopuses[j][i] = 0;
-        } */
+    for ( i = 0; i < maxY; i++) {
+      for ( j = 0; j < maxX; j++) {
+        dumboOctopuses[j][i].value += 1;
+          dumboOctopuses[j][i].isFlashing = 0;
+        if (dumboOctopuses[j][i].value > 9){
+          flashing = 1;
+
       }
     }
+  }
+  
+while (flashing){
+flashing = 0;
 
-    for (int i = 0; i < maxY; i++) {
-      for (int j = 0; j < maxX; j++) {
-        if (dumboOctopuses[j][i] > 9) {
-          dumboOctopuses[j][i] = 0;
-          if (i > 0) { /* N */
-            if (dumboOctopuses[j][i - 1] != 0) {
-              dumboOctopuses[j][i - 1]++;
+    for ( i = 0; i < maxY; i++) {
+      for ( j = 0; j < maxX; j++) {
+        if (dumboOctopuses[j][i].value > 9) {
+          dumboOctopuses[j][i].value = 0;
+          dumboOctopuses[j][i].isFlashing = 1;
+          flashes++;
+          flashing = 1;
+          if (i > 0) { 
+            if (dumboOctopuses[j][i - 1].isFlashing == 0) {
+              dumboOctopuses[j][i - 1].value++;
             }
-            if (dumboOctopuses[j][i - 1] > 9) {
-              dumboOctopuses[j][i - 1] = 0;
-            }
-            if (j > 0) { /* NE */
-              if (dumboOctopuses[j - 1][i - 1] != 0) {
-                dumboOctopuses[j - 1][i - 1]++;
-              }
-              if (dumboOctopuses[j - 1][i - 1] > 9) {
-                dumboOctopuses[j - 1][i - 1] = 0;
-              }
-            }
-            if (j < maxX - 1) { /* NW */
-              if (dumboOctopuses[j + 1][i - 1] != 0) {
-                dumboOctopuses[j + 1][i - 1]++;
-              }
-              if (dumboOctopuses[j + 1][i - 1] > 9) {
-                dumboOctopuses[j + 1][i - 1] = 0;
-              }
-            }
-          } /* Done Northers */
 
-          if (i < maxY - 1) { /* S */
-            if (dumboOctopuses[j][i + 1] != 0) {
-              dumboOctopuses[j][i + 1]++;
-            }
-            if (dumboOctopuses[j][i + 1] > 9) {
-              dumboOctopuses[j][i + 1] = 0;
-            }
-            if (j > 0) { /* SE */
-              if (dumboOctopuses[j - 1][i + 1] != 0) {
-                dumboOctopuses[j - 1][i + 1]++;
+            if (j > 0) {
+              if (dumboOctopuses[j - 1][i - 1].isFlashing == 0) {
+                dumboOctopuses[j - 1][i - 1].value++;
               }
-              if (dumboOctopuses[j - 1][i + 1] > 9) {
-                dumboOctopuses[j - 1][i + 1] = 0;
-              }
+
             }
-            if (j < maxX - 1) { /* SW */
-              if (dumboOctopuses[j + 1][i + 1] != 0) {
-                dumboOctopuses[j + 1][i + 1]++;
+            if (j < maxX - 1) { 
+              if (dumboOctopuses[j + 1][i - 1].isFlashing == 0) {
+                dumboOctopuses[j + 1][i - 1].value++;
               }
-              if (dumboOctopuses[j + 1][i + 1] > 9) {
-                dumboOctopuses[j + 1][i + 1] = 0;
-              }
+
             }
-          } /* Done southers */
+          } 
+
+          if (i < maxY - 1) {
+            if (dumboOctopuses[j][i + 1].isFlashing == 0) {
+              dumboOctopuses[j][i + 1].value++;
+            }
+
+            if (j > 0) {
+              if (dumboOctopuses[j - 1][i + 1].isFlashing == 0) {
+                dumboOctopuses[j - 1][i + 1].value++;
+              }
+
+            }
+            if (j < maxX - 1) { 
+              if (dumboOctopuses[j + 1][i + 1].isFlashing == 0) {
+                dumboOctopuses[j + 1][i + 1].value++;
+              }
+
+            }
+          } 
 
           if (j < maxX - 1) {
-            if (dumboOctopuses[j + 1][i] != 0) {
-              dumboOctopuses[j + 1][i]++;
+            if (dumboOctopuses[j + 1][i].isFlashing == 0) {
+              dumboOctopuses[j + 1][i].value++;
             }
-            if (dumboOctopuses[j + 1][i] > 9) {
-              dumboOctopuses[j + 1][i] = 0;
-            }
+
           }
 
           if (j > 0) {
-            if (dumboOctopuses[j - 1][i] != 0) {
-              dumboOctopuses[j - 1][i]++;
+            if (dumboOctopuses[j - 1][i].isFlashing == 0) {
+              dumboOctopuses[j - 1][i].value++;
             }
-            if (dumboOctopuses[j - 1][i] > 9) {
-              dumboOctopuses[j - 1][i] = 0;
-            }
+
           }
         }
+        
       }
     }
   }
-  for (int i = 0; i < maxY; i++) {
-    for (int j = 0; j < maxX; j++) {
-      printf("%d", dumboOctopuses[i][j]);
-    }
-    printf("\n");
-  }
-  return 2;
+   } 
+  
+  
+  return flashes;
 }
 
+
+int partTwo() {
+  char line[500];
+  struct Octopus dumboOctopuses[maxY][maxX];
+  int i = 0;
+  int j = 0;
+  int stepsToFlash = 0;
+  int syncFlash = 0;
+  struct Octopus o;
+  FILE* in_file = fopen(INPUT, "r");
+
+  if (in_file == NULL) {
+    printf("File doesnt exist\n");
+    return 1;
+  }
+  while (fgets(line, 500, in_file) != NULL) {
+    for (i = 0; i < maxX; i++) {
+      o.value = line[i] - '0';
+      o.isFlashing = 0;
+      dumboOctopuses[j][i] = o;
+    } 
+    j++;
+  } 
+
+
+  while (syncFlash == 0) {
+    int flashing = 0;
+    int i;
+    int j;
+    for ( i = 0; i < maxY; i++) {
+      for ( j = 0; j < maxX; j++) {
+        dumboOctopuses[j][i].value += 1;
+          dumboOctopuses[j][i].isFlashing = 0;
+        if (dumboOctopuses[j][i].value > 9){
+          flashing = 1;
+
+      }
+    }
+  }
+  
+while (flashing){
+flashing = 0;
+
+    for ( i = 0; i < maxY; i++) {
+      for ( j = 0; j < maxX; j++) {
+        if (dumboOctopuses[j][i].value > 9) {
+          dumboOctopuses[j][i].value = 0;
+          dumboOctopuses[j][i].isFlashing = 1;
+          flashing = 1;
+          if (i > 0) { 
+            if (dumboOctopuses[j][i - 1].isFlashing == 0) {
+              dumboOctopuses[j][i - 1].value++;
+            }
+
+            if (j > 0) {
+              if (dumboOctopuses[j - 1][i - 1].isFlashing == 0) {
+                dumboOctopuses[j - 1][i - 1].value++;
+              }
+
+            }
+            if (j < maxX - 1) { 
+              if (dumboOctopuses[j + 1][i - 1].isFlashing == 0) {
+                dumboOctopuses[j + 1][i - 1].value++;
+              }
+
+            }
+          } 
+
+          if (i < maxY - 1) {
+            if (dumboOctopuses[j][i + 1].isFlashing == 0) {
+              dumboOctopuses[j][i + 1].value++;
+            }
+
+            if (j > 0) {
+              if (dumboOctopuses[j - 1][i + 1].isFlashing == 0) {
+                dumboOctopuses[j - 1][i + 1].value++;
+              }
+
+            }
+            if (j < maxX - 1) { 
+              if (dumboOctopuses[j + 1][i + 1].isFlashing == 0) {
+                dumboOctopuses[j + 1][i + 1].value++;
+              }
+
+            }
+          } 
+
+          if (j < maxX - 1) {
+            if (dumboOctopuses[j + 1][i].isFlashing == 0) {
+              dumboOctopuses[j + 1][i].value++;
+            }
+
+          }
+
+          if (j > 0) {
+            if (dumboOctopuses[j - 1][i].isFlashing == 0) {
+              dumboOctopuses[j - 1][i].value++;
+            }
+
+          }
+        }
+        
+      }
+    }
+  }
+  syncFlash = 1;
+  for ( i = 0; i < maxY; i++) {
+    for ( j = 0; j < maxX; j++) {
+      if( dumboOctopuses[i][j].value != 0){
+        syncFlash = 0;
+      }
+    }
+  }
+  stepsToFlash++;
+   } 
+  return stepsToFlash;
+}
+
+
+
 int main() {
-  printf("%d", partOne());
+  printf("%d\n", partOne());
+    printf("%d\n", partTwo());
   return EXIT_SUCCESS;
 }
